3fa05f4020d310b970a26160a38c6db6ed2ede79
Yvan Epale <epaleyvan@gmail.com>
2024-01-05 20:27:45 +0100

    Correction of the storage services

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -18,55 +18,6 @@ export class StorageService {
       username: 'nsisse',
       password: 'password'
     };
-    let op1:Operation = {
-      type: OperationType.IN,
-      amount: 10000,
-      date: new Date(),
-      note: 'Entrée de base'
-    };
-    let op2:Operation = {
-      type: OperationType.IN,
-      amount: 5000,
-      date: new Date(),
-      note: 'Entrée de base'
-    };
-    let op3:Operation = {
-      type: OperationType.IN,
-      amount: 12000,
-      date: new Date(),
-      note: 'Entrée de base'
-    };
-    let op4:Operation = {
-      type: OperationType.OUT,
-      amount: -1000,
-      date: new Date(),
-      note: 'Entrée de base'
-    };
-    let op5:Operation = {
-      type: OperationType.LOAD,
-      amount: 5000,
-      date: new Date(),
-      note: 'Entrée de base'
-    };
-    let op6:Operation = {
-      type: OperationType.CREDIT,
-      amount: 10000,
-      date: new Date(),
-      note: 'Entrée de base'
-    };
-    let op7:Operation = {
-      type: OperationType.OUT,
-      amount: -5000,
-      date: new Date(),
-      note: 'Entrée de base'
-    };
-    let op8:Operation = {
-      type: OperationType.OUT,
-      amount: -4000,
-      date: new Date(),
-      note: 'Entrée de base'
-    };
-    //storage.set("listOperations_" + user.username, [op1, op2, op3, op4, op5, op6, op7, op8]);
     storage.set(user.username, user);
     this._storage = storage;
   }
